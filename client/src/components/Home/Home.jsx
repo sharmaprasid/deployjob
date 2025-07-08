ab1b38f81a34b74ba7f82c88d58e94e1218956fa
Prasid <prasidpoudelsharma@gmail.com>
2025-07-07 23:31:36 -0400

    error fixed

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         <HeroSection />
         <HowItWorks />
         <PopularCategories />
-        <PopularCompanies />
+        {/* <PopularCompanies /> */}
       </section>
     </>
   );
